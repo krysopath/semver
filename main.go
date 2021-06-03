acd88fa6bd71c3fcd18006859ad5a2e9324ccfba
Georg <krysopath@gmail.com>
2021-06-03 22:57:18 +0200

    add readme and beauty

--- main.go
+++ main.go
@@ -16,27 +16,25 @@ var (
 	isSorting = flag.Bool("sort", false, "sort strings from $* inputs with semver algo")
 )
 
-type SemanticVersionString string
+type SerialzedSemVer struct {
+	Canonical  string `json:"canonical"`
+	Major      string `json:"major"`
+	MajorMinor string `json:"majorminor"`
+	PreRelease string `json:"prerelease"`
+	Build      string `json:"build"`
+	Source     string `json:"source"`
+}
 
 type SemanticVersion struct {
 	value string
 }
 
-func (s *SemanticVersion) Canonical() string {
-	return semver.Canonical(s.value)
-}
-func (s *SemanticVersion) Major() string {
-	return semver.Major(s.value)
-}
-func (s *SemanticVersion) MajorMinor() string {
-	return semver.MajorMinor(s.value)
-}
-func (s *SemanticVersion) Prerelease() string {
-	return semver.Prerelease(s.value)
-}
-func (s *SemanticVersion) Build() string {
-	return semver.Build(s.value)
-}
+func (s *SemanticVersion) Canonical() string  { return semver.Canonical(s.value) }
+func (s *SemanticVersion) Major() string      { return semver.Major(s.value) }
+func (s *SemanticVersion) MajorMinor() string { return semver.MajorMinor(s.value) }
+func (s *SemanticVersion) Prerelease() string { return semver.Prerelease(s.value) }
+func (s *SemanticVersion) Build() string      { return semver.Build(s.value) }
+
 func (s SemanticVersion) String() string {
 	asJson, _ := s.MarshalJSON()
 	data, _ := json.Marshal(asJson)
@@ -44,21 +42,15 @@ func (s SemanticVersion) String() string {
 }
 
 func (s SemanticVersion) MarshalJSON() ([]byte, error) {
-	return json.Marshal(struct {
-		Canonical  string `json:"canonical"`
-		Major      string `json:"major"`
-		MajorMinor string `json:"majorminor"`
-		PreRelease string `json:"prerelease"`
-		Build      string `json:"build"`
-		Source     string `json:"source"`
-	}{
-		Canonical:  s.Canonical(),
-		Major:      s.Major(),
-		MajorMinor: s.MajorMinor(),
-		PreRelease: s.Prerelease(),
-		Build:      s.Build(),
-		Source:     s.value,
-	})
+	return json.Marshal(
+		SerialzedSemVer{
+			Canonical:  s.Canonical(),
+			Major:      s.Major(),
+			MajorMinor: s.MajorMinor(),
+			PreRelease: s.Prerelease(),
+			Build:      s.Build(),
+			Source:     s.value,
+		})
 }
 
 type ByVersion []SemanticVersion
@@ -68,22 +60,23 @@ func (a ByVersion) Less(i, j int) bool { return semver.Compare(a[i].value, a[j].
 func (a ByVersion) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 
 func outputSingle(data string) {
-	trimmed := strings.TrimSpace(data)
-	semver := SemanticVersion{trimmed}
-
-	fmt.Fprintln(os.Stdout, semver)
+	ver := SemanticVersion{data}
+	out, _ := json.Marshal(ver)
+	fmt.Fprintln(
+		os.Stdout,
+		string(out),
+	)
 }
 
 func outputSorted(data string) {
-	trimmed := strings.TrimSpace(data)
-	vers := strings.Split(trimmed, " ")
+	vers := strings.Split(data, " ")
 	var sorted []SemanticVersion
 	for _, v := range vers {
 		sorted = append(sorted, SemanticVersion{v})
 	}
 	sort.Sort(ByVersion(sorted))
-	out, _ := json.Marshal(sorted)
 
+	out, _ := json.Marshal(sorted)
 	fmt.Fprintln(os.Stdout, string(out))
 }
 
@@ -92,15 +85,16 @@ func input() string {
 	if err != nil {
 		panic(err)
 	}
-	var version string
+	var data string
 
 	if fi.Mode()&os.ModeNamedPipe == 0 {
-		version = os.Getenv("CI_COMMIT_TAG")
+		data = os.Getenv("CI_COMMIT_TAG")
 	} else {
 		reader := bufio.NewReader(os.Stdin)
-		version, _ = reader.ReadString('\n')
+		data, _ = reader.ReadString('\n')
 	}
-	return version
+	trimmed := strings.TrimSpace(data)
+	return trimmed
 }
 
 func main() {
---
+++ readme.md
@@ -0,0 +1,89 @@
+# semver
+
+> semantic versions in pipelines
+
+
+`semver` was created to serve as reliable pipeline helper. Born out of
+necessity and distrust towards regex.
+
+
+## Installation
+
+```
+go get github.com/krysopath/semver@v0.1.2
+```
+
+## Usage
+
+Parse semantic version from stdin:
+```
+$ echo "v0.1.23-alpha2+9999" | ./semver | jq
+{
+  "build": "+9999",
+  "canonical": "v0.1.23-alpha2",
+  "major": "v0",
+  "majorminor": "v0.1",
+  "prerelease": "-alpha2"
+}
+```
+
+Parse many versions and sort them:
+```
+$ echo 'v3 v1.1.1-pre0+999 v3.1.1-dest0 v2 v3.1.1' | go run . -sort | jq
+[
+  {
+    "canonical": "v1.1.1-pre0",
+    "major": "v1",
+    "majorminor": "v1.1",
+    "prerelease": "-pre0",
+    "build": "+999",
+    "source": "v1.1.1-pre0+999"
+  },
+  {
+    "canonical": "v2.0.0",
+    "major": "v2",
+    "majorminor": "v2.0",
+    "prerelease": "",
+    "build": "",
+    "source": "v2"
+  },
+  {
+    "canonical": "v3.0.0",
+    "major": "v3",
+    "majorminor": "v3.0",
+    "prerelease": "",
+    "build": "",
+    "source": "v3"
+  },
+  {
+    "canonical": "v3.1.1-dest0",
+    "major": "v3",
+    "majorminor": "v3.1",
+    "prerelease": "-dest0",
+    "build": "",
+    "source": "v3.1.1-dest0"
+  },
+  {
+    "canonical": "v3.1.1",
+    "major": "v3",
+    "majorminor": "v3.1",
+    "prerelease": "",
+    "build": "",
+    "source": "v3.1.1"
+  }
+]
+```
+
+When run inside Gitlab Runners, can execute without stdin:
+```
+$ export CI_COMMIT_TAG=v1.22.4-some+123
+$ ./semver | jq
+{
+  "build": "+123",
+  "canonical": "v1.22.4-some",
+  "major": "v1",
+  "majorminor": "v1.22",
+  "prerelease": "-some"
+}
+
+```

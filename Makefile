1867a992a5334569483d87961b932a6941fd1d8f
Georg vom Endt <6668411+krysopath@users.noreply.github.com>
2022-08-23 23:47:27 +0200

    fix: removes sorting from semver cli, added release increment

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
-TAG = $(shell git tag | sort -r --version-sort | head -n1)
+TAG = '$(shell git tag | sort -r --version-sort | head -n1)'
 SEMVERS = $(shell echo $(TAG) | semver)
 
 major:
 	@set -e;\
 	export major=$$(( $$(echo '$(SEMVERS)'|jq -r .major|sed 's/^v//') + 1 )); \
-	git tag -m "v$$major.0.0 major release" --sign v$$major.0.0
+	git tag  v$$major.0.0
 minor:
 	@set -e;\
 	export major=$$(( $$(echo '$(SEMVERS)'|jq -r .major|sed 's/^v//') + 0 )); \
@@ -12,11 +12,11 @@ minor:
 	export patch=$$(( $$(echo '$(SEMVERS)' | jq -r .canonical|sed 's/^v[0-9]*\.[0-9]*\.//') + 0 ));\
 	git tag -m "v$$major.$$minor.$$patch minor release" --sign v$$major.$$minor.$$patch
 patch:
-	@set -e;\
+	@set -ex;\
 	export major=$$(( $$(echo '$(SEMVERS)'|jq -r .major|sed 's/^v//') + 0 )); \
 	export minor=$$(( $$(echo '$(SEMVERS)' | jq -r .majorminor | sed -e 's/^v[0-9]*\.//' -e 's/\.[0-9]$$//' ) + 0 ));\
 	export patch=$$(( $$(echo '$(SEMVERS)' | jq -r .canonical|sed 's/^v[0-9]*\.[0-9]*\.//') + 1 ));\
-	git tag -m "v$$major.$$minor.$$patch patch fix" --sign v$$major.$$minor.$$patch
+	echo git tag -m "v$$major.$$minor.$$patch patch fix" --sign v$$major.$$minor.$$patch
 
 semver:
 	git tag -f -m '$(TAG)' --sign $$(echo '$(SEMVERS)' | jq -r .major)
@@ -26,5 +26,8 @@ release:
 	git push
 	git push --tags -f
 
-install:
+tests:
+	go test ./...
+	
+install: tests
 	go install ./cmd/...
--- versions/main.go
+++ versions/main.go
@@ -2,16 +2,11 @@ package versions
 
 import (
 	"encoding/json"
-	"flag"
 	"fmt"
+	"strconv"
+	"strings"
 
 	"golang.org/x/mod/semver"
-	"gopkg.in/alessio/shellescape.v1"
-)
-
-var (
-	isSorting  = flag.Bool("sort", false, "sort strings from $* inputs with semver algo")
-	emitFormat = flag.String("format", "json", "output format on stdout")
 )
 
 type (
@@ -68,13 +63,55 @@ func (s SemanticVersion) MarshalEVAL() ([]byte, error) {
 	//		out = fmt.Sprintf("%s\nexport MAJOR='%s'", out, shellescape.Quote(s.Major()))
 	//	}
 
-	out = fmt.Sprintf("%s\nexport MAJOR='%s'", out, shellescape.Quote(string(s.Major())))
-	out = fmt.Sprintf("%s\nexport MAJORMINOR='%s'", out, shellescape.Quote(string(s.MajorMinor())))
-	out = fmt.Sprintf("%s\nexport CANONICAL='%s'", out, shellescape.Quote(string(s.Canonical())))
-	out = fmt.Sprintf("%s\nexport PRERELEASE='%s'", out, shellescape.Quote(string(s.Prerelease())))
-	out = fmt.Sprintf("%s\nexport BUILD='%s'", out, shellescape.Quote(string(s.Build())))
+	out = fmt.Sprintf("%s\nexport MAJOR='%s'", out, string(s.Major()))
+	out = fmt.Sprintf("%s\nexport MAJORMINOR='%s'", out, string(s.MajorMinor()))
+	out = fmt.Sprintf("%s\nexport CANONICAL='%s'", out, string(s.Canonical()))
+	out = fmt.Sprintf("%s\nexport PRERELEASE='%s'", out, string(s.Prerelease()))
+	out = fmt.Sprintf("%s\nexport BUILD='%s'", out, string(s.Build()))
 
 	return []byte(out), nil
+}
+
+func (s *SemanticVersion) Release(rType string) SemanticVersion {
+	switch rType {
+	case "major":
+		major, err := strconv.Atoi(strings.TrimPrefix(string(s.Major()), "v"))
+		if err != nil {
+			panic(err)
+		}
+		major += 1
+		return SemanticVersion{fmt.Sprintf("v%d.0.0", major)}
+	case "minor":
+		major, err := strconv.Atoi(strings.TrimPrefix(string(s.Major()), "v"))
+		if err != nil {
+			panic(err)
+		}
+		minor, err := strconv.Atoi(strings.TrimPrefix(string(s.MajorMinor()), fmt.Sprintf("v%d.", major)))
+		if err != nil {
+			panic(err)
+		}
+		minor += 1
+		return SemanticVersion{fmt.Sprintf("v%d.%d.0", major, minor)}
+	case "patch":
+		major, err := strconv.Atoi(strings.TrimPrefix(string(s.Major()), "v"))
+		if err != nil {
+			panic(err)
+		}
+		minor, err := strconv.Atoi(strings.TrimPrefix(string(s.MajorMinor()), fmt.Sprintf("v%d.", major)))
+		if err != nil {
+			panic(err)
+		}
+		noPrefix := strings.TrimPrefix(string(s.Canonical()), fmt.Sprintf("v%d.%d.", major, minor))
+		patch := strings.TrimSuffix(noPrefix, string(s.Prerelease()))
+		newPatch, err := strconv.Atoi(patch)
+		if err != nil {
+			panic(err)
+		}
+		newPatch += 1
+		return SemanticVersion{fmt.Sprintf("v%d.%d.%d", major, minor, newPatch)}
+	default:
+		panic(fmt.Sprintf("err: %s unkown release type", rType))
+	}
 }
 
 type ByVersion []SemanticVersion
